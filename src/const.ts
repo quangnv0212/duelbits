1d1fc51d9468d193b43ad233a81d9306825134ab
Vu Quang <quangnv.0212@gmail.com>
2024-05-18 16:41:45 +0700

    update

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,6 @@
 export const TELEGRAM_BOT_TOKEN =
   "7068353230:AAH3A7IIJvM75HO2OBCkWz6es2kUVhai8dQ";
-export const CHATID = "@6775540763";
+export const CHATID = "6775540763";
+// export const TELEGRAM_BOT_TOKEN =
+//   "6232366821:AAGMFKtsJo2jvVxupBHA8bRWHhixf9RqgQM";
+// export const CHATID = "@etopfunnoti";
